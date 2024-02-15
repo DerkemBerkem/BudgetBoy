7cd3af40de534922972394e760343e21c08d1340
Derek Brandon <dbrandon@berkeley.edu>
2024-02-14 18:48:05 -0800

    Working on login

--- src/basicCRUD/MainPage.jsx
+++ src/basicCRUD/MainPage.jsx
@@ -25,6 +25,12 @@ const getInitialData = () => {
 export default function MainPage() {
   const [users, setUsers] = useState(getInitialData);
   const [postContent, setPostContent] = useState("");
+  const [activeUser, setActiveUser] = useState(initialUser);
+  const [loginUser, setLoginUser] = useState({
+    username: initialUser.username,
+    password: initialUser.password,
+  });
+
   console.log(users);
 
   const handlePost = (e) => {
@@ -49,6 +55,23 @@ export default function MainPage() {
     justifyContent: "center",
   };
 
+  const findUser = (username, password) => {
+    const foundUsers = users.filter((u) => {
+      return u.username == username && u.password == password;
+    });
+    if (foundUsers.length == 0) {
+      return null;
+    }
+    return foundUsers[0];
+  };
+
+  const handleLogin = (e) => {
+    const foundUser = findUser(loginUser.username, loginUser.password);
+    if (foundUser != null) {
+      setActiveUser(foundUser);
+    }
+  };
+
   const myCenterPad = {
     display: "flex",
     justifyContent: "center",
@@ -59,36 +82,63 @@ export default function MainPage() {
   };
 
   return (
-    <div>
-      <div style={myCenter}>
-        <h1>SUCK MY BLOG</h1>
+    <>
+      <div>
+        <input
+          onChange={(e) =>
+            setLoginUser((old) => {
+              return { ...old, username: e.target.value };
+            })
+          }
+          type="text"
+          name="Login"
+          id="login"
+          value={activeUser.username}
+        />
+        <input
+          onChange={(e) =>
+            setLoginUser((old) => {
+              return { ...old, password: e.target.value };
+            })
+          }
+          type="text"
+          name="Login"
+          id="login"
+          value={activeUser.password}
+        />
+        <button onClick={handleLogin}>Login</button>
       </div>
       <div>
+        <div style={myCenter}>
+          <h1>SUCK MY BLOG</h1>
+        </div>
         <div>
-          <h3 style={myCenter}>{users.length > 0 && users[0].username}</h3>
-        </div>
-        <div style={myCenter}>
-          <input
-            onChange={(e) => setPostContent(e.target.value)}
-            type="text"
-            name="Post"
-            id="post"
-            value={postContent}
-          />
-          <button onClick={handlePost}>Add post</button>
-        </div>
-        <div style={myCenter}>
           <div>
-            {users.length > 0 &&
-              users[0].posts.length > 0 &&
-              users[0].posts.toReversed().map((c) => (
-                <div style={myCenterPad} key={c.id}>
-                  {c.content}
-                </div>
-              ))}
+            <h3 style={myCenter}>{users.length > 0 && users[0].username}</h3>
+          </div>
+          <div style={myCenter}>
+            <input
+              onChange={(e) => setPostContent(e.target.value)}
+              type="text"
+              name="Post"
+              id="post"
+              value={postContent}
+            />
+            <button onClick={handlePost}>Add post</button>
+          </div>
+          <div style={myCenter}>
+            <div>
+              {users.length > 0 &&
+                activeUser.posts.length > 0 &&
+                activeUser.posts.toReversed().map((c) => (
+                  <div style={myCenterPad} key={c.id}>
+                    {c.content}
+                  </div>
+                ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
